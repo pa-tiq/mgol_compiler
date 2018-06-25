0c801dd08ba8605418a214609705fb2355090400
Patrick <patrickdsilva99@gmail.com>
2018-06-24 23:46:52 -0300

    funcionando

---
+++ mgol_compiler/out.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+typedef char literal[256];
+void main(void)
+{
+	literal A;
+	int B;
+	int D;
+	double C;
+
+
+
+	printf("Digite B:");
+	scanf("%d", B);
+	printf("Digite A:");
+	scanf("%s", A);
+T0 = B>2;
+
+if ( T0 )
+{
+
+T1 = B<=4;
+
+if ( T1 )
+{
+
+	printf("B esta entre 2 e 4");
+}
+
+}
+
+B = B+4;
+
+B = B+2;
+
+B = B+3;
+
+D = B;
+
+C = 50.2E-3;
+
+	printf("\nB=\n");
+	printf(D);
+	printf("\n");
+	printf(C);
+	printf("\n");
+	printf(A);
