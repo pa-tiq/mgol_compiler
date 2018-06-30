4553ab99f70b2a81648cb15573f0f1ad1d5f1031
Patrick <patrickdsilva99@gmail.com>
2018-06-30 19:09:22 -0300

    identação do código c atualizada

--- mgol_compiler/out.c
+++ mgol_compiler/out.c
@@ -13,30 +13,30 @@ void main(void)
 	scanf("%d", B);
 	printf("Digite A:");
 	scanf("%s", A);
-T0 = B>2;
+	T0 = B>2;
 
-if ( T0 )
-{
+	if ( T0 )
+	{
 
-T1 = B<=4;
+	T1 = B<=4;
 
-if ( T1 )
-{
+	if ( T1 )
+	{
 
 	printf("B esta entre 2 e 4");
-}
+	}
 
-}
+	}
 
-B = B+4;
+	B = B+4;
 
-B = B+2;
+	B = B+2;
 
-B = B+3;
+	B = B+3;
 
-D = B;
+	D = B;
 
-C = 50.2E-3;
+	C = 50.2E-3;
 
 	printf("\nB=\n");
 	printf(D);
@@ -44,3 +44,4 @@ C = 50.2E-3;
 	printf(C);
 	printf("\n");
 	printf(A);
+}
