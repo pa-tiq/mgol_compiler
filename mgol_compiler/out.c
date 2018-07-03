fe313591de01ca327acd2a70b99c37b29f8f4f43
Patrick Dantas <patrick.dantas@inventsoftware.com.br>
2018-07-03 14:38:19 -0300

    correções na geração de código c

--- mgol_compiler/out.c
+++ mgol_compiler/out.c
@@ -6,42 +6,37 @@ void main(void)
 	int B;
 	int D;
 	double C;
-
-
+	int T0;
+	int T1;
+	int T2;
+	int T3;
+	int T4;
 
 	printf("Digite B:");
-	scanf("%d", B);
+	scanf("%d", &B);
 	printf("Digite A:");
 	scanf("%s", A);
 	T0 = B>2;
-
 	if ( T0 )
 	{
-
-	T1 = B<=4;
-
-	if ( T1 )
-	{
-
-	printf("B esta entre 2 e 4");
+		T1 = B<=4;
+		if ( T1 )
+		{
+			printf("B esta entre 2 e 4");
+		}
 	}
-
-	}
-
-	B = B+4;
-
-	B = B+2;
-
-	B = B+3;
-
-	D = B;
-
+	T2 = B+4;
+	B = T2;
+	T3 = B+2;
+	B = T3;
+	T4 = B+3;
+	B = T4;
+	D = A;
 	C = 50.2E-3;
-
 	printf("\nB=\n");
-	printf(D);
+	printf("%d", D);
 	printf("\n");
-	printf(C);
+	printf("%lf", C);
 	printf("\n");
-	printf(A);
+	printf("%s", A);
 }
